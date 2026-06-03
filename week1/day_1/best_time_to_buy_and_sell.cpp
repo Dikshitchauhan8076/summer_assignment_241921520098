19e226e87fd9f77c935144e5ffc9f45c56937660
Dikshit_Chauhan8076 <dikshitchauhanrock2006@gmail.com>
2026-06-03 13:38:05 +0530

    Add week1 day1 solutions

---
+++ week1/day_1/best_time_to_buy_and_sell.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit=0,smallest=prices[0];
+        for(int i=1;i<prices.size();i++){
+            if(prices[i]<smallest){
+                smallest=prices[i];
+            }
+            else{
+                profit=max(profit,prices[i]-smallest);
+            }
+        }
+        return profit;
+    }
+};
